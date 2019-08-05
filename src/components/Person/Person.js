10e312c0e9b6a1fcb9af52624bd317d4f67c77a8
ZeFifi <ph.pinceloup@protonmail.com>
2019-08-05 17:27:48 +0200

    Added style with CSS

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import "./Person.css";
+
 const Person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>Hi, my name is {props.name} and I'm {props.age}.</p>
             <input type="text" onChange={props.edit} placeholder={props.name}/>
         </div>
