e60f2988af5772f684500b4e6572c07674cf024a
ZeFifi <ph.pinceloup@protonmail.com>
2019-08-05 15:43:16 +0200

    Used state and props to render different components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 import Person from "./components/Person/Person";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            people : [
+                {
+                    name: "Philippe",
+                    age: 31
+                },
+                {
+                    name: "Sylvie",
+                    age: 30
+                }
+            ]
+        }
+    }
 
   render() {
     return (
         <div className="App">
           <h1>Hello people!</h1>
-          <Person name="Philippe" age="31"/>
-          <Person name="Sylvie" age="30"/>
+          <Person name={this.state.people[0].name} age={this.state.people[1].name} />
+          <Person name={this.state.people[1].name} age={this.state.people[1].age} />
         </div>
     );
   }
