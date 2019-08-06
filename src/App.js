a4ec80aa8a601468718f4112ec959e4c1871da76
ZeFifi <ph.pinceloup@protonmail.com>
2019-08-06 16:02:07 +0200

    We can edit the name of a specific card now

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ class App extends Component {
         this.state = {
             people : [
                 {
+                    id: 1,
                     name: "Philippe",
                     age: 31
                 },
                 {
+                    id: 2,
                     name: "Sylvie",
                     age: 30
                 },
                 {
+                    id: 3,
                     name: "Hervé",
                     age: 31
                 }
@@ -23,6 +26,30 @@ class App extends Component {
             isVisible: false
         }
     }
+
+    editNamesHandler = (event, id) => {
+        // We get the ID of the person when typing in the input
+        const personIndex = this.state.people.findIndex(person => {
+            return person.id === id;
+        });
+
+        // We create an object "person" at this specific index
+        const person = {
+            ...this.state.people[personIndex]
+        };
+
+        // We set a new person.name depending on what's typed in the input
+        person.name = event.target.value;
+
+        // We copy the original array
+        const people = [...this.state.people];
+
+        // This specific person becomes the new person
+        people[personIndex] = person;
+
+        // The state is updated
+        this.setState({people: people})
+    };
 
     toggleNames = () => {
       this.setState({
@@ -42,12 +69,14 @@ class App extends Component {
            cards = (
               <div className="people">
                   {
-                      this.state.people.map((person, index) => {
-                      return <Person key={index}
+                      this.state.people.map((person, id) => {
+                      return <Person
+                          key={person.id}
                           name={person.name}
                           age={person.age}
-                          click={() => this.deleteHandler(index)}
+                          click={() => this.deleteHandler(id)}
                           placeholder={person.name}
+                          edit={(event) => this.editNamesHandler(event, person.id)}
                       />
                   })
                   }
